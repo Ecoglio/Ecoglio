61ec0a2116f3d1827d7707e4ac1399f90033d6db
TuNisiAa-guy <112637330+TuNisiAa-guy@users.noreply.github.com>
2023-11-30 21:22:30 +0100

    Add files via upload

---
+++ it/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta>
+        <title>Ecoglio : libri usati</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header>ECOGLIO</header>
+        <nav>
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./about.php">About</a></li>
+                <li><a href="./supporto.php">Supporto</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
